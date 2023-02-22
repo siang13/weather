a988912b55ff6f8b74f057b87713f4a131bf8841
siang13 <joosiang13@gmail.com>
2023-02-22 22:40:26 +0800

    init setup

---
+++ src/core/config.ts
@@ -0,0 +1,2 @@
+export const API_URI = process.env.NEXT_PUBLIC_API_ENDPOINT;
+export const REQUEST_TIMEOUT = 30000;
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => (
+  <>
+    <Head>
+      <title>Weather</title>
+      <meta content="Weather" name="keywords" />
+    </Head>
+    <div className="bg-slate-600">123</div>
+  </>
+);
+
+export default Home;
